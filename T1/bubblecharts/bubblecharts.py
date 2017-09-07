436aa04c656f6809b201e3a3fc610b2614a1ffa2
crabarca <caabarca1@uc.cl>
2017-09-07 02:22:43 -0300

    finished bubblecharts

--- T1/bubblecharts/bubblecharts.py
+++ T1/bubblecharts/bubblecharts.py
@@ -3,14 +3,14 @@
 import svgwrite
 import json
 import os
-import operator
+import math 
 
 rootDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 input_file = ''
 outfile1 = os.path.join(rootDir, 'bubblecharts/bubblechart1.svg')
 outfile2 = os.path.join(rootDir, 'bubblecharts/bubblechart2.svg')
 canvas_height = '800px'
-canvas_width = '1200px'
+canvas_width = '1300px'
 
 def parse_data(filename):
     with open(filename, 'r') as infile:
@@ -41,7 +41,7 @@ def draw_axis_x(canvas,data):
         canvas.add(canvas.text(text=round(scale * i, 2) , insert=(xStartLabel, yStartLabel)))
         xStartLabel += xLabelOffset
 
-    canvas.add(canvas.text(text = 'HDI', insert = (1020, 705)))
+    canvas.add(canvas.text(text = 'Hdi', insert = (1020, 705)))
     
     return label_position
 
@@ -71,12 +71,42 @@ def draw_axis_y(canvas, data):
     
     return label_position
 
-def draw_bubblechart(canvas, data, xLabels, yLabels):
+def draw_bubblechart(canvas, data, xLabels, yLabels, tipo):
     sortedArea = list(reversed(sort_by_value(data, 'area')))
     for pais in sortedArea:
+        color = get_color(data, tipo, data[pais[0]][tipo])
         x, y = get_coordinates(xLabels, yLabels, data[pais[0]]['hdi'], data[pais[0]]['gini'])
-        drw.add(drw.circle(center=(x,y), r=3, fill="black", stroke_width=10, stroke="green"))
+        radio = math.sqrt(data[pais[0]]['area'] / math.pi) / 25 # este es el factor de reduccion
+        drw.add(drw.circle(center=(x,y), r=radio, fill=color, stroke_width=1, stroke='black'))
 
+def draw_title(canvas, text, x, y):
+    canvas.add(canvas.text(text=text , insert=(x, y), style="font-size:30"))
+
+def draw_simbology(canvas, data, tipo):
+    # agregamos cuadrados 
+    xRectStart = 1100
+    yRectStart = 300
+    yRectOffset = 40
+
+    xLabelStart = 1150
+    continentes = ['Americas', 'Asia', 'Oceania', 'Europe', 'Africa']
+    religiones = ['Islam', 'Christianity', 'Shinto', 'Syncretic', 'Buddhism', 'Animist', 'Judaism', 'Hindu']
+    if tipo == 'continente':
+        for i in range(5):
+            color = get_color(data, tipo, continentes[i])
+            canvas.add(canvas.rect(insert=(xRectStart,yRectStart), size=(50, 30), fill = color))
+            canvas.add(canvas.text(text=continentes[i] , insert=(xLabelStart, yRectStart + 20), style="font-size:15"))
+            yRectStart += yRectOffset
+
+    elif tipo == 'religion':
+        for i in range(8):
+            color = get_color(data, tipo, religiones[i])
+            canvas.add(canvas.rect(insert= (xRectStart, yRectStart), size=(50,30), fill = color))
+            canvas.add(canvas.text(text=religiones[i], insert = (xLabelStart, yRectStart + 20), style="font-size:15"))
+            yRectStart += yRectOffset
+
+
+# FUNCIONES AUXILIARES PARA OBTENER CARACTERISTICAS A DIBUJAR
 def get_coordinates(xLabels, yLabels, x, y):
     # retorna la posicion en pixeles donde va a estar x,y (hdi, gini) en funcion de los labels del canvas
     # se calcula la posicion definiendo dos ecuaciones de la recta  
@@ -88,11 +118,21 @@ def get_coordinates(xLabels, yLabels, x, y):
 
     return xPixel, yPixel
 
-def get_color(data, continente):
-    colors = {'America': 'rgb(222,106,89)', 'Africa': 'rgb(77,77,77)' , 'Asia':'rgb(222,177,89)' , 'Oceania':'rgb(43,135,134)' , 'Europe': 'rgb(222,140,89)'}
-    
-    return colors[continente]
+def get_color(data, tipo, valor):
+    if tipo == 'continente':
+        colors = {'Americas': 'rgb(222,106,89)', 'Africa': 'rgb(77,77,77)' , 
+                  'Asia':'rgb(222,177,89)' , 'Oceania':'rgb(43,135,134)' , 
+                  'Europe': 'rgb(222,140,89)'}
+        return colors[valor]
+    elif tipo == 'religion':
+        colors = {'Syncretic': 'rgb(222,140,89)', 'Islam': 'rgb(222,106,89)', 
+                  'Christianity': 'rgb(222,177,89)',
+                  'Judaism': 'rgb(222,89,139)', 'Hindu':'rgb(43,135,88)' ,  
+                  'Shinto': 'rgb(43,135,134)',
+                  'Buddhism': 'rgb(77,77,77)', 'Animist': 'rgb(201,222,89)'}
 
+        return colors[valor]
+       
 
 def sort_by_value(data, value):
     countries_value = {}
@@ -102,23 +142,38 @@ def sort_by_value(data, value):
     sorted_value = sorted(countries_value.items(), key=lambda x : x[1])
     return sorted_value
 
+
 if __name__ == "__main__":
     data = parse_data(os.path.join(rootDir, 'processed_data/bubblecharts.json'))
-    drw = svgwrite.Drawing(outfile1, size=(canvas_width, canvas_height))
-
-    gini = []
-    hdi = []
-    for pais in data.values():
-        gini.append(pais['gini'])
-        hdi.append(pais['hdi'])
     
-    print('x axis', draw_axis_x(drw, data))
-    print('y axis', draw_axis_y(drw, data))
+    # BUBBLECHART CONTINENTAL 
+    drw = svgwrite.Drawing(outfile1, size=(canvas_width, canvas_height))
+    # print(data)
+    
     xLabels = draw_axis_x(drw, data)
     yLabels = draw_axis_y(drw, data)
 
-    draw_bubblechart(drw, data, xLabels, yLabels)
-
+    draw_bubblechart(drw, data, xLabels, yLabels, 'continente')
+    draw_title(drw, 'Color según continente', 400, 100)
+    draw_simbology(drw, data, 'continente')
 
     drw.save()
+
+    religion = sort_by_value(data, 'religion')
+    relig = [item[1] for item in religion]
+    count = [[x,relig.count(x)] for x in set(relig)]
+    
+#  BUBBLECHART RELIGIOSO
+    drw = svgwrite.Drawing(outfile2, size=(canvas_width, canvas_height))
+    # print(data)
+    
+    xLabels = draw_axis_x(drw, data)
+    yLabels = draw_axis_y(drw, data)
+
+    draw_bubblechart(drw, data, xLabels, yLabels, 'religion')
+    draw_title(drw, 'Color según religión', 400, 100)
+    draw_simbology(drw, data, 'religion')
+
+    drw.save()
+    
     
