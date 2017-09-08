5752dbc58c1bd95ef078cf6a35638fd20c0216fe
crabarca <caabarca1@uc.cl>
2017-09-07 23:12:42 -0300

    added simbology and title to heatmap

--- T1/heatmap/heatmap.py
+++ T1/heatmap/heatmap.py
@@ -18,7 +18,7 @@ def parseData(filename):
     return data 
 
 def labelYaxis(canvas):
-    y_offset = 50
+    y_offset = 55
     y_start = 210
     x_start = 30
     continentes = ['Africa', 'Europa', 'America', 'Asia', 'Oceania']
@@ -27,10 +27,10 @@ def labelYaxis(canvas):
         y_start += y_offset
 
 def labelXaxis(canvas):
-    x_offset = 70
+    x_offset = 83
     x_start = 120
     y_start = 480
-    for religion in data['Religions']:
+    for religion in data['major_relig_names']:
         text = canvas.text(text=religion, insert=(x_start, y_start))
                             #    style='transform:rotate(10deg) translate(50px, 0px)')
         canvas.add(text)
@@ -38,14 +38,40 @@ def labelXaxis(canvas):
 
 def drawAxis(canvas):
     y_axis = [(90,150), (90, 450)]
-    x_axis = [(90, 450), (850, 450)]
+    x_axis = [(90, 450), (950, 450)]
     
-    canvas.add(canvas.line(start=y_axis[0], end=y_axis[1], stroke="black", stroke_width = 3))
-    canvas.add(canvas.line(start=x_axis[0], end=x_axis[1] ,stroke="black", stroke_width = 3))
+    canvas.add(canvas.line(start=y_axis[0], end=y_axis[1], stroke="black", stroke_width = 1))
+    canvas.add(canvas.line(start=x_axis[0], end=x_axis[1] ,stroke="black", stroke_width = 1))
     
-def getColors(data, religion, continente):
+def drawRect(canvas, x, y, height, width, color):
+    canvas.add(canvas.rect(insert=(x,y), size=(width,height), fill=color))
+
+def draw_simbology(canvas):
+    xRectStart = 325
+    yRectStart = 525
+    xRectOffset = 70 
+    yLabelStart = 570
+
+    colors = {1 : 'rgb(254, 236, 108)', 2: 'rgb(249, 204, 41)', 
+              3:'rgb(252, 154, 43)' , 4:'rgb(232, 80, 4)' , 
+              5:'rgb(188,21,12)' ,6:'rgb(166, 37, 16)' }
+    segmentos = [round(100*i/6, 2) for i in range(7)]
+
+    for i in range(1,7):
+        canvas.add(canvas.rect(insert=(xRectStart,yRectStart), size=(50, 30), fill = colors[i]))
+        canvas.add(canvas.text(text= '< ' + str(segmentos[i]) + '%', insert=(xRectStart,  yLabelStart), style="font-size:15"))
+        xRectStart += xRectOffset
+
+def draw_title(canvas, title, x, y):
+    canvas.add(canvas.text(text=title , insert=(x, y), style="font-size:30"))
+
+
+def get_colors(data, religion, continente):
     # color 1 es mas claro y 6 mas oscuro
-    colors = {1 : 'rgb(254, 236, 108)', 2: 'rgb(249, 204, 41)', 3:'rgb(252, 154, 43)' , 4:'rgb(232, 80, 4)' , 5:'rgb(188,21,12)' ,6:'rgb(166, 37, 16)' }
+    colors = {1 : 'rgb(254, 236, 108)', 2: 'rgb(249, 204, 41)', 
+              3:'rgb(252, 154, 43)' , 4:'rgb(232, 80, 4)' , 
+              5:'rgb(188,21,12)' ,6:'rgb(166, 37, 16)' }
+
     segmentos = [round(100*i/6, 2) for i in range(7)]
 
     pop_continente = data['Population'][continente]
@@ -63,26 +89,23 @@ def getColors(data, religion, continente):
             return colors[i+1]
 
         
-def drawRect(canvas, x, y, height, width, color):
-    canvas.add(canvas.rect(insert=(x,y), size=(width,height), fill=color))
-
 def drawHeatmap(canvas, data):
     # dibujamos los 50 rectangulos 
     rect_height = 55
-    rect_width = 70
+    rect_width = 80
     x_start = 110
-    y_start = 175
-    x_offset = 70
-    y_offset = 55
+    y_start = 162
+    x_offset = 83
+    y_offset = 58
     continentes = ['africa', 'europe', 'america', 'asia', 'oceania']
 
-    for religion in data['Religions']:
+    for religion in data['religions_code']:
         for continente in continentes:
-            color = getColors(data, religion, continente)
+            color = get_colors(data, religion, continente)
             print(continente, religion)
             drawRect(canvas, x_start, y_start, rect_height, rect_width, color)
             y_start += y_offset
-        y_start = 175
+        y_start = 162
         x_start += x_offset
 
 if __name__ == "__main__":
@@ -97,6 +120,7 @@ if __name__ == "__main__":
     labelXaxis(drw)
 
     drawHeatmap(drw, data)
-    
+    draw_title(drw, 'Porcentaje relativo de participación', 300, 120)
+    draw_simbology(drw)
 
     drw.save()
