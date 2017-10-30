df2b55ae32a9112ec65d96e98582eeb9b6e11ae7
crabarca <caabarca1@uc.cl>
2017-10-29 21:42:29 -0300

    changed colors

--- T3/scripts/graph.js
+++ T3/scripts/graph.js
@@ -1,6 +1,6 @@
 
-const WIDTH = 1400;
-const HEIGHT = 1000;
+const WIDTH = 1100;
+const HEIGHT = 750;
 const MARGIN = { TOP: 40, BOTTOM: 40, LEFT: 50, RIGHT: 50 };
 
 const width = WIDTH - MARGIN.RIGHT - MARGIN.LEFT;
@@ -10,8 +10,10 @@ const FILEPATH = 'datasets/dataset.json';
 
 const radius = 20;
 
-const MAX_RADIUS = 60;
+const MAX_RADIUS = 50;
 
+const forkColors0 = d3.schemeGreens[7].reverse();
+const forkColors = forkColors0.slice(0,6);
 const container = d3.select('#container1')
   .append('svg')
     .attr('width', WIDTH)
@@ -20,33 +22,28 @@ const container = d3.select('#container1')
     .attr('transform',
         `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
 
-const ticked = () => {
-    container.selectAll('.node')
-    // .attr('transform', node => `translate(${node.x}, ${node.y})`)
-    .attr('cx', node => Math.max(radius, Math.min(width - radius, node.x)))
-    .attr('cy', node => Math.max(radius, Math.min(height - radius, node.y)));
 
-    container.selectAll('.links')
-             .attr('x1', link => Math.max(radius, Math.min(width - radius, link.source.x)))
-             .attr('y1', link => Math.max(radius, Math.min(height - radius, link.source.y)))
-             .attr('x2', link => Math.max(radius, Math.min(width - radius, link.target.x)))
-             .attr('y2', link => Math.max(radius, Math.min(height - radius, link.target.y)));
-};
 
 const infoBox = container.append('text');
 
 const simulation = d3.forceSimulation()
                      .force('center', d3.forceCenter(width/2, height/2))
-                     .force('collision', d3.forceCollide(60))
-                     .force('charge', d3.forceManyBody().strength(-500))
-                     .force('link', d3.forceLink().id(node => node.name));
+                     .force('collision', d3.forceCollide()
+                                           .radius([40]))
+                     .force('charge', d3.forceManyBody().strength(-600))
+                     .force('link', d3.forceLink().id(node => node.name)
+                                                  .distance(100));
 
 // Escalas!
 const rscale = d3.scalePow().exponent(0.2)
                   .range([0, MAX_RADIUS]);
 
-const forkScale = d3.scalePow().exponent(0.2)
-                    .range([0, 20]);
+const forkScale = d3.scaleOrdinal().range(forkColors);
+
+//Tooltip
+var divTooltip = d3.select("#container1").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
 
 d3.json(FILEPATH, dataset => {
@@ -57,25 +54,28 @@ d3.json(FILEPATH, dataset => {
               .links(dataset.links)
               .distance(MAX_RADIUS + 20);
 
+
     // Rangos de escala
     rscale.domain([0, d3.max(dataset.nodes, d => parseInt(d.stars))])
-    forkScale.domain([0, d3.max(dataset.nodes, d =>parseInt(d.forks))])
+    forkScale.domain([d3.max(dataset.nodes, d =>parseInt(d.forks)), 0])
 
     // Flechas para los links
-    container.append("svg:defs").selectAll("marker")
-      .data(['false'])
-      .enter().append("svg:marker")
+    container.append("svg:defs")
+      .append("svg:marker")
         .attr("id", 'arrowhead')
         .attr("viewBox", "0 -5 10 10")
-        .attr("refX", 45)
-        .attr("refY", -1.5)
+        .attr("refX", 60)
+        .attr("refY", -0)
         .attr("markerWidth", 5)
         .attr("markerHeight", 5)
         .attr("orient", "auto")
-      .append("path")
-        .attr("d", "M0,-5L10,0L0,5");
+        .attr('xoverflow','visible')
+        .attr('fill', '#979797')
 
-    container.selectAll('.links')
+      .append("path")
+        .attr("d", "M0,-5 L10,0 L0,5");
+
+    const links = container.selectAll('.links')
              .data(dataset.links)
              .enter()
              .append('g')
@@ -86,18 +86,11 @@ d3.json(FILEPATH, dataset => {
               .attr('x2', link => link.target.x)
               .attr('y2', link => link.target.y)
               .style('stroke', link => linkColor(link.dev))
+              .style('stroke-width', '2px')
               .attr("id", function(d,i) {return 'edge'+i})
               .attr('marker-end','url(#arrowhead)')
               .style("pointer-events", "none");
 
-    // // add the links and the arrows
-    // container.append("svg:g").selectAll(".links")
-    //          .data(dataset.links)
-    //          .enter()
-    //          .append("svg:path")
-    // //    .attr("class", function(d) { return "link " + d.type; })
-    //          .attr("class", "links")
-    //          .attr("marker-end", "url(#end)");
 
     const nodes = container.selectAll('.node')
                            .data(dataset.nodes)
@@ -106,25 +99,83 @@ d3.json(FILEPATH, dataset => {
                             .append('circle')
                               .attr('class', 'node')
                               .attr('r', d=> rscale(d.stars))
-                              .attr('fill', 'blue')
-                              .attr('stroke', 'orange')
-                              .attr('stroke-width', d=> forkScale(d.forks))
+                              .attr('fill', d=>forkScale(d.forks))
                               .call(drag_handler);
 
     nodes.append('text').text(node => node.name).attr('dy', 5).attr('fill', 'black');
 
+    // para seleccionar nodos vecinos
+    var linkedByIndex = {};
+    dataset.links.forEach(function(d) {
+          linkedByIndex[d.source.index + "," + d.target.index] = 1;
+        });
 
+    function neighboring(a, b) {
+      return linkedByIndex[a.index + "," + b.index];
+    }
+    console.log(linkedByIndex);
+    nodes.on('mouseover',(d, i, node) => {
+          // d3.select(nodes[i])
+          console.log(d);
+            d3.select(nodes[i]).style("opacity", function(o) {
+              return neighboring(d, o) ? 1 : 0.7;
+              });
 
+            d3.selectAll('circle')
+            .filter(':not(:hover)')
+            .style('fill-opacity', 0.3)
+            .style('');
+
+            divTooltip.transition()
+            .duration(200)
+            .style("opacity", 0.9)
+            .style("position", "absolute");
+
+            divTooltip.html(d.name + "<br/>"
+                          + "<b>Stars: </b>"
+                          + d.stars + "<br/>" + "<b>Forks: </b>"
+                          +d.forks)
+
+            .style('font-size', '20px')
+            .style("left", 1100  + "px")
+            .style("top", 100 +   "px");
+            })
+
+          .on('mouseout', (d, i, nodes) => {
+              d3.select(nodes[i])
+                .style('stroke-width', d=>forkScale(d.forks));
+
+                divTooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+
+                d3.selectAll('circle')
+                .style('fill-opacity', 1);
+          });
+
+    function ticked() {
+      container.selectAll('.node')
+      // .attr('transform', node => `translate(${node.x}, ${node.y})`)
+      .attr('cx', node => Math.max(radius, Math.min(width - radius, node.x)))
+      .attr('cy', node => Math.max(radius, Math.min(height - radius, node.y)));
+
+      container.selectAll('.links')
+          .attr('x1', link => Math.max(radius, Math.min(width - radius, link.source.x)))
+          .attr('y1', link => Math.max(radius, Math.min(height - radius, link.source.y)))
+          .attr('x2', link => Math.max(radius, Math.min(width - radius, link.target.x)))
+          .attr('y2', link => Math.max(radius, Math.min(height - radius, link.target.y)));
+
+    };
 });
 
 
 // Helper functions
 const linkColor = link_type => {
   if (link_type){
-    return 'red';
+    return '#ef8a62';
   }
   else {
-    return 'green';
+    return '#bdbdbd';
   }
 };
 
